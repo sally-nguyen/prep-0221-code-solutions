8920ca04a463ce6ef5b7a0718596861faa753934
Sally Nguyen <sahleex3@gmail.com>
2021-02-25 00:36:07 -0800

    addTwoNumbers Exercise

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,11 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(addTwoNumbers(4, 4));
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+console.log(addTwoNumbers(2, 2));
+var addTwoNumbersResult = addTwoNumbers(2,2);
+console.log("addTwoNumbers Exercise:", addTwoNumbersResult);
