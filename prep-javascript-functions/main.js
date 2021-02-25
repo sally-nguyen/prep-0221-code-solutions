959a7f38e0de74402ad3dcd8c2d83e43899511f9
Sally Nguyen <sahleex3@gmail.com>
2021-02-25 01:30:31 -0800

    multiplyAndDivideBy5 Exercise

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -29,3 +29,10 @@ function addAndMultiplyBy5(num1, num2) {
 console.log(addAndMultiplyBy5(10,5));
 var addAndMultiplyBy5Result = addAndMultiplyBy5(10,5);
 console.log("addAndMultiplyBy5 Exercise:", addAndMultiplyBy5Result);
+
+function multiplyAndDivideBy5(num1,num2) {
+  return (num1*num2) / 5;
+}
+console.log(multiplyAndDivideBy5(35,10));
+var multiplyAndDivideBy5Result = multiplyAndDivideBy5(35,10);
+console.log("multiplyAndDivideBy5 Exercise:", multiplyAndDivideBy5Result);
